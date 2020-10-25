b852df38dfc25855703b4c8438c79ca7125d31fb
Timih124 <tim.hackel24@gmail.com>
2020-10-24 21:48:34 -0500

    adding the questions to java folder

---
+++ javascript/questions.js
@@ -0,0 +1,74 @@
+var questions = [
+    {
+      title: "Which Chicago sport team won a championship?",
+      choices: ["Bulls", "Bears", "Cubs", "Fire"],
+      answer: "Cubs"
+    },
+    {
+      title: "What Lake Does Chicago Touch? ",
+      choices: [
+        "Lake Michigan",
+        "Lake Superior",
+        "Lake Huron",
+        "Lake Erie"
+      ],
+      answer: "Lake Michigan"
+    },
+    {
+      title: "What School Is Not Found Near Chicago?",
+      choices: ["Depaul", "Loyola", "NorthWestern", "Duke"],
+      answer: "Duke"
+    },
+    {
+      title: "Who Did Not Play for the Chicago Bulls",
+      choices: [
+        "Derrick Rose",
+        "Michael Jordan",
+        "Sammy Sosa",
+        "Scottie Pippen"
+      ],
+      answer: "Sammy Sosa"
+    },
+    {
+      title:
+        "How many square miles doe Chicago rest on?",
+      choices: [
+        "234",
+        "100",
+        "23",
+        "165"
+      ],
+      answer: "234"
+    },
+    {
+      title: "What famous celebrity is from Chicago?",
+      choices: [
+        "Chance the rapper",
+        "Brad Pitt",
+        "Sandra Bullock",
+        "Mila Kunis"
+      ],
+      answer: "Chance the rapper"
+    },
+    {
+      title: "What is the actually name of the tallest building in Chicago?",
+      choices: ["Willis Tower", "John Hancock", "Merch Mart", "Navy Pier"],
+      answer: "Willis Tower"
+    },
+    {
+      title: "What do Chicagoians call it?",
+      choices: [
+        "Sears Tower",
+        "Fields Tower",
+        "Chase Tower",
+        "Tall Tower"
+      ],
+      answer: "Sears Tower"
+    },
+    {
+      title:
+        "What is the name of the river that runs through Chicago's downtown?",
+      choices: ["Illinois River", "I & M Canal", "Mississippi River", "Chicago River"],
+      answer: "Chicago River"
+    }
+];
